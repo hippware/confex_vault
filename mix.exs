ee097b96c8d0dc8fd5b651f0b8200c4ec0c1f4c7
Bernard Duggan <bernard@hippware.com>
2019-09-16 13:35:29 +1000

    Add stuff required for hex publishing

--- mix.exs
+++ mix.exs
@@ -1,13 +1,18 @@
 defmodule ConfexVault.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :confex_vault,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.9",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      description: description(),
+      deps: deps(),
+      docs: docs(),
+      start_permanent: Mix.env() == :prod
     ]
   end
 
@@ -25,6 +30,29 @@ defmodule ConfexVault.MixProject do
       {:distillery, "~> 2.0"},
       {:module_config, "~> 1.0"},
       {:vaultex, "~> 0.12"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Bernard Duggan"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/hippware/confex_vault"}
+    ]
+  end
+
+  defp description do
+    """
+    A set of modules to allow Vault secrets to be retrieved as part of a
+    Confex-based configuration system
+    """
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      main: "readme",
+      extras: ["README.md"]
     ]
   end
 end
